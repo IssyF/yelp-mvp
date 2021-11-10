3cdf4e572bdb2968861f23382a87aef66fc552a2
Isabel Foo <isabel.foo@hotmail.com>
2021-11-10 12:26:00 +0000

    routes added and restaurants controller created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'clearing restaurants database'
+Restaurant.destroy_all
+
+10.times do
+  puts 'creating restaurant'
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "created restaurant #{restaurant.id}"
+end
